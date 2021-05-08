5cc7d125e33edaa20e39ae0071fc14e69cd61765
MaahinPlayz <69353377+MaahinPlayz@users.noreply.github.com>
2021-05-08 17:01:22 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import{createAppContainer} from "react-navigation"
+import {createStackNavigator}from "react-navigation-stack"
+import HomeScreen from "./screens/Home"
+import DetailScreen from "./screens/Details"
+
+
+export default function App() {
+  return (
+   <AppContainer/>
+  );
+}
+  const appStackNavigator=createStackNavigator(
+    {
+    Home:{
+      screen:HomeScreen,
+      navigationOptions:{
+        headershown:false
+      }
+    },
+    Details:{
+      screen:DetailScreen
+    }
+  },
+  {
+  initialRouteName:"Home"
+  }
+  );
+const AppContainer=createAppContainer(appStackNavigator);
+
+
+
